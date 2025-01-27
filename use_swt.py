9b37a401664782a99038001411bec028c72b38cf
owencking <92190360+owencking@users.noreply.github.com>
2025-01-26 23:10:40 -0500

    Full rewrite of stand-alone (outside of clams-kitchen) visaid processing module

--- use_swt.py
+++ use_swt.py
@@ -13,6 +13,7 @@ import argparse
 import os
 import sys
 import warnings
+import json
 from pprint import pprint
 
 # Import local modules
@@ -20,61 +21,188 @@ import proc_swt
 import create_visaid
 import lilhelp
 
-MAX_GAP = 180000
+
+def display_tfs(tfs:list):
+    """
+    This function simply prints a simple table of TimeFrame annotations from a tfs table
+    """
+    tfs_pretty = []
+    for f in tfs:
+        tfs_pretty += [[ f"{f[2]:08}", 
+                         lilhelp.tconv(f[2]), 
+                         lilhelp.tconv(f[3]), 
+                         f[1] ]]
+    pprint(tfs_pretty)
+
+
+
+def proc_display(mmif_path:str):
+    """
+    This function simply prints a simple table of TimeFrame annotations from the MMIF file
+    """
+
+    # Load up the MMIF serialization from a MMIF file
+    with open(mmif_path, "r") as usefile:
+        mmif_str = usefile.read()
+
+    print("Attempting to process MMIF into a scene list...")
+
+    # Get the right views
+    tp_view_id, tf_view_id = proc_swt.get_swt_view_ids(mmif_str)
+
+    # create TimeFrame table from the serialized MMIF
+    tfs = proc_swt.tfs_from_mmif( mmif_str, 
+                                  tp_view_id=tp_view_id,
+                                  tf_view_id=tf_view_id )
+
+    print(len(tfs), "scenes labeled in")
+    print(mmif_path, ":\n")
+
+    display_tfs(tfs)
+
+    print()
+
+
+
+def proc_visual( mmif_path:str, 
+                 video_path:str, 
+                 visaid_path:str=None, 
+                 stdout:bool=False,
+                 scene_adj:bool=True,
+                 cust_params:dict={} ):
+    """
+    This performs all the steps to process a MMIF file and create a visaid.
+    """
+
+    #
+    # Collect and sort all the declared and default parameters        
+    #
+    if not cust_params:
+        cust_params = {}
+
+    proc_swt_params = {}
+    for key in proc_swt.PROC_SWT_DEFAULTS:
+        if key in cust_params:
+            proc_swt_params[key] = cust_params[key]
+        else:
+            proc_swt_params[key] = proc_swt.PROC_SWT_DEFAULTS[key]
+    visaid_params = {}
+    for key in create_visaid.VISAID_DEFAULTS:
+        if key in cust_params:
+            visaid_params[key] = cust_params[key]
+        else:
+            visaid_params[key] = create_visaid.VISAID_DEFAULTS[key]
+
+    
+    # Load up the MMIF serialization from a MMIF file
+    with open(mmif_path, "r") as usefile:
+        mmif_str = usefile.read()
+
+    if not stdout:
+        print("Attempting to process MMIF into a scene list...")
+
+    # Get the right views
+    tp_view_id, tf_view_id = proc_swt.get_swt_view_ids(mmif_str)
+
+    # create TimeFrame table from the serialized MMIF
+    tfs = proc_swt.tfs_from_mmif( mmif_str, 
+                                  tp_view_id=tp_view_id,
+                                  tf_view_id=tf_view_id )
+
+    # find the outer temporal boundaries of the TimePoint analysis
+    first_time, final_time = proc_swt.first_final_time_in_mmif( mmif_str, tp_view_id=tp_view_id )
+
+    # Create an adjusted TimeFrame table (with scenes added and/or removed)
+    if scene_adj:
+        tfs_adj = proc_swt.adjust_tfs( tfs, first_time, final_time, proc_swt_params )
+    else:
+        tfs_adj = tfs[:]
+
+    mmif_metadata_str = proc_swt.get_mmif_metadata_str( mmif_str,
+                                                        tp_view_id,
+                                                        tf_view_id )
+
+    # Assign values for other required parameters
+    if visaid_path:
+        output_dirname, hfilename = os.path.split(visaid_path)
+    else:
+        output_dirname, hfilename = ".", ""
+
+    _, video_filename = os.path.split(video_path)
+
+
+    if not stdout:
+        print("Creating a visual index...")
+
+    _, visaid_path = create_visaid.create_visaid( 
+        video_path=video_path, 
+        tfs=tfs_adj, 
+        stdout=stdout, 
+        output_dirname=output_dirname,
+        hfilename=hfilename,
+        item_id=video_filename,
+        proc_swt_params=proc_swt_params,
+        visaid_params=visaid_params,
+        mmif_metadata_str=mmif_metadata_str
+        )
+
+    if not stdout:
+        print("Visual index created at")
+        print(visaid_path)
+
+
+
 
 def main():
-    
     app_desc="""
     Creates useful data artifacts from the output of the SWT detection CLAMS app.
     """
 
-    parser = parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         prog='python use_swt.py',
         description=app_desc,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("mmif_path", metavar="MMIF",
+    parser.add_argument("mmif_path", metavar="MMIF", type=str,
         help="Path and filename for the MMIF file")
-    parser.add_argument("video_path", metavar="VIDEO", nargs="?",
+    parser.add_argument("video_path", metavar="VIDEO", type=str, nargs="?",
         help="Path and filename for the video file")
     parser.add_argument("-d", "--display", action="store_true",
         help="Output a summary index of TimeFrames from MMIF")
-    parser.add_argument("-e", "--extract", action="store_true",
-        help="Extract representative stills from each TimeFrame and save them in a directory (requires video file)")
     parser.add_argument("-v", "--visual", action="store_true",
         help="Create an HTML file with a visual index of TimeFrames (requires video file)")
     parser.add_argument("-s", "--stdout", action="store_true",
-        help="Outputs the visual index HTML to stdout instead of writing a file.  Implies 'visual'. Implies not 'display' or 'extract'.")
-    parser.add_argument("-w", "--warn", action="store_true",
-        help="Display (do not suppres) Python warning messages")
+        help="Outputs the visual index HTML to stdout instead of writing a file.  Implies 'visual'. Implies not 'display'.")
+    parser.add_argument("-o", "--visaid_path", type=str, default=None,
+        help="File path for visual index HTML file.  Implies 'visual'.")
+    parser.add_argument("-m", "--mmif_only", action="store_true",
+        help="Include only MMIF TimeFrames (do not adjust scenes according to customizations) before creating a visual index")
+    parser.add_argument("-c", "--customization", type=str, default=None,
+        help="Path to a JSON file supplying the values of customization options")
     
     args = parser.parse_args() 
-    #print(args)
 
-    if not args.warn:
-        warnings.filterwarnings("ignore")
-
-    # make argument values consistent
+    # Make argument values consistent
     stdout = args.stdout
     if stdout:
         display = False
-        extract = False
         visual = True
+        warn = False
     else:
         display = args.display
-        extract = args.extract
         visual = args.visual
 
-    # validate positional arguments
+    # Validate non-boolean  arguments
     mmif_path = args.mmif_path
-    video_path = args.video_path
     if not os.path.exists(mmif_path):
         print("Error:  Invalid file path for MMIF file.")
         print("Run with '-h' for help.")
         sys.exit(1)
-    if visual or extract:
+
+    video_path = args.video_path
+    if visual:
         if video_path is None:
-            print("Error:  Video file path must be provided for 'visual' or 'extract'.")
+            print("Error:  Video file path must be provided for 'visual'.")
             print("Run with '-h' for help.")
             sys.exit(1)
         elif not os.path.exists(video_path):
@@ -82,77 +210,61 @@ def main():
             print("Run with '-h' for help.")
             sys.exit(1)
 
-    # Load up the MMIF serialization from a MMIF file
-    usefile = open(mmif_path, "r")
-    mmifstr = usefile.read()
-    usefile.close()
+    visaid_path = args.visaid_path
+    if visual and visaid_path:
+        output_dirname, hfilename = os.path.split(visaid_path)
+        if not os.path.exists(output_dirname):
+            print("Error:  No directory exists corresponding to visaid file path.")
+            print("Run with '-h' for help.")
+            sys.exit(1)
 
-    # Process the serialized MMIF to create the table of time frames
-    if visual:
-        tfs = proc_swt.list_tfs(mmifstr, max_gap=MAX_GAP, include_endframe=True)
+    # Validate customization file
+    cust_path = args.customization
+    if cust_path:
+        if not os.path.exists(cust_path):
+            print("Error:  No file exists at the supplied customization file path.")
+            print("Run with '-h' for help.")
+            sys.exit(1)
+
+    if args.mmif_only:
+        scene_adj = False
     else:
-        tfs = proc_swt.list_tfs(mmifstr)
+        scene_adj = True
 
-    # Display the TimeFrame index
-    if display: 
-        # create prettier table
-        tfs_pretty = []
-        for f in tfs:
-            tfs_pretty += [[ f"{f[2]:08}", 
-                             lilhelp.tconv(f[2]), 
-                             lilhelp.tconv(f[3]), 
-                             f[1] ]]
 
-        print(len(tfs), "scenes labeled in")
-        print(mmif_path, ":")
-        print()
-        pprint(tfs_pretty)
-        print()
+    # Call the appropriate procedures. 
+    # (These are not mutually exclusive.)
 
-    # Extract stills that are representatives of each TimeFrame
-    if extract :
-        print("Extracting representative stills for TimeFrame annotations...")
-        exlist = []
-        for row in tfs:
-            #if row[1] == 'chyron' or row[1] == 'credits' :
-            exlist.append(row[4])  # representative timepoint for the TimeFrame (in ms)
+    # suppress warnings that come from the Mmif module
+    warnings.filterwarnings("ignore")
 
-        # title project based on the name of the video file
-        vfilename = os.path.basename(video_path)
-        fname, ext = os.path.splitext(vfilename)
-        
-        lilhelp.extract_stills(video_path, exlist, fname)
-        print("Done.")
+    if display:
+        proc_display(mmif_path)
 
-    # Make visual aid for cataloging
     if visual:
-        if not stdout:
-            print("Creating a visual index of TimeFrame annotations...")
-
-        # Title the project based on the name of the MMIF file
-        mfilename = os.path.basename(mmif_path)
-        proj_name, ext = os.path.splitext(mfilename)
-
-        # If the filename begins with "cpb-aacip", then assume that
-        # the guid is the filename without the extension.
-        vfilename = os.path.basename(video_path)
-        vfname, ext = os.path.splitext(vfilename)
-        if  vfname[:9] == "cpb-aacip" :
-            guid = vfname
+        if cust_path:
+            with open(cust_path, "r") as file:
+                cust_params = json.load(file)
+            if not stdout:
+                # Warn about spurious parameters
+                for key in cust_params:
+                    if key not in { **proc_swt.PROC_SWT_DEFAULTS, 
+                                    **create_visaid.VISAID_DEFAULTS } :
+                        print("Warning: `" + key + "` is not a valid config option for this postprocess. Ignoring.")
         else:
-            guid = None
+            cust_params = {}
 
-        create_visaid.create_visaid(
-            video_path=video_path, 
-            tfs=tfs, 
-            stdout=stdout, 
-            guid=guid,
-            id_in_filename=False)
+        proc_visual( mmif_path, 
+                     video_path, 
+                     visaid_path=visaid_path,
+                     stdout=stdout, 
+                     scene_adj=scene_adj,
+                     cust_params=cust_params )
 
-        if not stdout:
-            print("Done.")
-        
 
+#
+# Call to main function for stand-alone execution
+#
 if __name__ == "__main__":
     main()
 
